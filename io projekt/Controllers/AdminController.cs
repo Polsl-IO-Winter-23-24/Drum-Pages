0910bcedecd09e810e539d03cfd5b2f4481ecf33
maks3211 <maks02inf@gmail.com>
2024-02-07 17:59:28 +0100

    Przygotowanie pod panel admina

---
+++ io projekt/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using io_projekt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using System.Diagnostics;
+using System.Threading;
+using Thread = io_projekt.Models.Thread;
+
+
+
+namespace io_projekt.Controllers
+{
+    public class AdminController: Controller
+    {
+
+
+
+
+
+
+    }
+}
